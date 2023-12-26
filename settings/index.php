06903ff816f7eb72ca19cf6f728699890c4f43c5
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-12-27 00:58:08 +0700

    Add files via upload

--- settings/index.php
+++ settings/index.php
@@ -149,7 +149,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
     <?php include('../bootstrapcss.php'); ?>
-    <link rel="icon" type="image/png" href="../contents/favicon.svg">
+    <link rel="icon" type="image/png" href="../pictures/<?php echo !empty($user['picture']) ? $user['picture'] : '../contents/profile.jpg'; ?>">
   </head>
   <body>
     <div class="container mt-3 mb-5">
@@ -158,51 +158,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <h2 class="fw-bold text-center mt-5 mb-5">Edit Profile</h2>
       <form method="post" enctype="multipart/form-data">
         <div class="mb-2 row">
-          <label for="username" class="col-md-4 col-form-label text-nowrap fw-medium">Username :</label>
-          <div class="col-md-8">
+          <label for="username" class="col-md-2 col-form-label text-nowrap fw-medium">Username :</label>
+          <div class="col-md-10">
             <input type="text" name="username" class="form-control fw-bold text-white" value="<?php echo $user['username']; ?>">
           </div>
         </div>
         <div class="mb-2 row">
-          <label for="password" class="col-md-4 col-form-label text-nowrap fw-medium">Password :</label>
-          <div class="col-md-8">
+          <label for="password" class="col-md-2 col-form-label text-nowrap fw-medium">Password :</label>
+          <div class="col-md-10">
             <input type="password" name="password" class="form-control fw-bold text-white" value="<?php echo $user['password']; ?>">
           </div>
         </div>
         <div class="mb-2 row">
-          <label for="bio" class="col-md-4 col-form-label text-nowrap fw-medium">Bio :</label>
-          <div class="col-md-8">
+          <label for="bio" class="col-md-2 col-form-label text-nowrap fw-medium">Bio :</label>
+          <div class="col-md-10">
             <textarea name="bio" class="form-control fw-bold text-white vh-100" oninput="stripHtmlTags(this)"><?php echo $user['bio'] !== null ? strip_tags($user['bio']) : ''; ?></textarea>
           </div>
         </div>
         <div class="mb-2 row">
-          <label for="born" class="col-md-4 col-form-label text-nowrap fw-medium">Born :</label>
-          <div class="col-md-8">
+          <label for="born" class="col-md-2 col-form-label text-nowrap fw-medium">Born :</label>
+          <div class="col-md-10">
             <input type="date" name="born" class="form-control fw-bold text-white" value="<?php echo $user['born']; ?>">
           </div>
         </div>
         <div class="mb-2 row">
-          <label for="region" class="col-md-4 col-form-label text-nowrap fw-medium">Region :</label>
-          <div class="col-md-8">
+          <label for="region" class="col-md-2 col-form-label text-nowrap fw-medium">Region :</label>
+          <div class="col-md-10">
             <input type="text" name="region" class="form-control fw-bold text-white" value="<?php echo $user['region']; ?>">
           </div>
         </div>
         <div class="mb-2 row">
-          <label for="picture" class="col-md-4 col-form-label text-nowrap fw-medium">Profile Picture :</label>
-          <div class="col-md-8">
+          <label for="picture" class="col-md-2 col-form-label text-nowrap fw-medium">Profile Picture :</label>
+          <div class="col-md-10">
             <input type="file" name="profile_picture" class="form-control fw-bold text-white">
           </div>
         </div>
         <div class="mb-2 row">
-          <label for="background" class="col-md-4 col-form-label text-nowrap fw-medium">Background :</label>
-          <div class="col-md-8">
+          <label for="background" class="col-md-2 col-form-label text-nowrap fw-medium">Background :</label>
+          <div class="col-md-10">
             <input type="file" name="background_image" class="form-control fw-bold text-white">
           </div>
         </div>
         <?php for ($i = 1; $i <= 10; $i++) : ?>
           <div class="mb-2 row">
-            <label for="link<?php echo $i; ?>" class="col-md-4 col-form-label text-nowrap fw-medium">Link <?php echo $i; ?> :</label>
-            <div class="col-md-8">
+            <label for="link<?php echo $i; ?>" class="col-md-2 col-form-label text-nowrap fw-medium">Link <?php echo $i; ?> :</label>
+            <div class="col-md-10">
               <input type="text" name="link<?php echo $i; ?>" class="form-control fw-bold text-white" value="<?php echo $user['link' . $i]; ?>">
             </div>
           </div>
